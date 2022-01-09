b0cd8fda7ec1c127ea77360edfefc247c02a096d
SV-Pletneva <bg026@yandex.ru>
2022-01-10 00:44:45 +0500

    task_3

---
+++ script-2.sql
@@ -0,0 +1,33 @@
+create table if not exists genre (
+	genre_id serial primary key,
+	name varchar (40) not null
+	
+);
+
+
+create table if not exists artist (
+	artist_id serial primary key,
+	name varchar(40) unique not null,
+	genre integer references genre (genre_id)
+);
+
+create table if not exists album (
+	album_id serial primary key,
+	name varchar(100) not null,
+	year date not null,
+	artist_id integer references artist(artist_id)
+);
+
+create table if not exists tracks (
+	track_id serial primary key,
+	title text not null,
+	track_time time,
+	album_id integer references album(album_id)
+);
+
+create table if not exists collection (
+	collection_id serial primary key,
+	name varchar (100) not null,
+	year date not null,
+	track_id integer references tracks(track_id)
+);
